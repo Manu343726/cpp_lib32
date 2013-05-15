6e9e8ddc80d77495214cf1425f2bf1dc39ffb2c2
Manu343726 <Manu343726@gmail.com>
2013-05-15 18:18:52 +0200

    dl32TypeList<Ts...>::pop_front

--- code/headers/utils/loki/dl32Typing.h
+++ code/headers/utils/loki/dl32Typing.h
@@ -379,6 +379,8 @@ struct dl32TypeList<HEAD,TAIL...>
     
     template<typename T>
     using push_front = dl32TypeList<T,HEAD,TAIL...>; ///< Pushes front a new type to the typelist (Returns new typelist).
+    
+    using pop_front = dl32TypeList<TAIL...>; ///< Pops the begining type of the typelist (Returns new typelist).)
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
