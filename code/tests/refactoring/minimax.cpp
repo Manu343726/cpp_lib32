fbb1bbba9bf4efa69b42baa185db81544dec4155
Manu343726 <Manu343726@gmail.com>
2013-06-13 15:30:31 +0200

    Compile time minimax fixed

--- code/tests/refactoring/minimax.cpp
+++ code/tests/refactoring/minimax.cpp
@@ -55,6 +55,10 @@ enum TileValue
 	EMPTY_TILE
 };
 
+const TileValue X = PLAYER1;
+const TileValue O = PLAYER2;
+const TileValue e = EMPTY_TILE;
+
 template<TileValue value>
 using Tile = dl32ValueWrapper<TileValue,value>;
 
@@ -68,6 +72,9 @@ template<typename BOARD , TileValue PLAYER>
 struct PlayerWins
 {
 private:
+    template<typename _BOARD>
+    struct _wins;
+    
     template<TileValue UPLEFT , TileValue UP , TileValue UPRIGHT , TileValue LEFT , TileValue CENTER , TileValue RIGHT , TileValue DOWNLEFT , TileValue DOWN , TileValue DOWNRIGHT>
     struct _wins<Board<UPLEFT,UP,UPRIGHT,LEFT,CENTER,RIGHT,DOWNLEFT,DOWN,DOWNRIGHT>> : public dl32BoolWrapper< 
     
@@ -101,18 +108,26 @@ private:
     template<typename... GENERATED_BOARDS , typename CURRENT_TILE , typename... NEXT_TILES , typename... ORIGINAL_BOARD_PROCESSED_TILES>
     struct _generate<dl32TypeList<GENERATED_BOARDS...> , dl32TypeList<CURRENT_TILE,NEXT_TILES...>,ORIGINAL_BOARD_PROCESSED_TILES...>
     {
-        using generated_board = typename dl32tmp_if<CURRENT_TILE::value == EMPTY_TILE , // Player can play with this tile?
-                                                    dl32TypeList<ORIGINAL_BOARD_PROCESSED_TILES...,LAST_PLAYER,NEXT_TILES...>, //YES (Play with it, puts its mark)
-                                                    dl32TypeList<ORIGINAL_BOARD_PROCESSED_TILES...,CURRENT_TILE,NEXT_TILES...> //NO (Not plays with it, leave it as is)
-                                                   >::type;
+        using generated_board       = dl32TypeList<ORIGINAL_BOARD_PROCESSED_TILES...,LAST_PLAYER,NEXT_TILES...>;
+        using next_generated_boards = typename dl32tmp_if<CURRENT_TILE::value == EMPTY_TILE ,
+                                                          dl32TypeList<GENERATED_BOARDS...,generated_board>,
+                                                          dl32TypeList<GENERATED_BOARDS...> 
+                                                         >::type;
                 
-        using result = typename _generate<dl32TypeList<GENERATED_BOARDS...,generated_board> , dl32TypeList<NEXT_TILES...> , ORIGINAL_BOARD_PROCESSED_TILES...,CURRENT_TILE>::result;
+        using result = typename _generate<next_generated_boards , dl32TypeList<NEXT_TILES...> , ORIGINAL_BOARD_PROCESSED_TILES...,CURRENT_TILE>::result;
+    };
+    
+    template<typename... GENERATED_BOARDS , typename... ORIGINAL_BOARD_PROCESSED_TILES>
+    struct _generate<dl32TypeList<GENERATED_BOARDS...> , dl32EmptyTypeList ,ORIGINAL_BOARD_PROCESSED_TILES...>
+    {               
+        using result = dl32TypeList<GENERATED_BOARDS...>;
     };
     
 public:
     using result = typename _generate<dl32EmptyTypeList,BOARD>::result;
 };
 
+/*
 template<typename INIT_BOARD , TileValue PLAYER , unsigned int MAX_DEPTH = 2> 
 class MINIMAX
 {
@@ -147,10 +162,71 @@ private:
         using result = typename dl32tmp_if<is_leaft,CURRENT_BOARD,typename _try_next_boards<typename GenerateNextBoards<CURRENT_BOARD,CURRENT_PLAYER>::result>::result,
     };
 };
+ * */
+
+const string to_string_table[] = {"X","O","."};
+
+const string to_string(TileValue value)
+{
+    return to_string_table[(unsigned int)value];
+}
+
+template<typename BOARD>
+struct print;
+
+template<TileValue UPLEFT , TileValue UP , TileValue UPRIGHT , TileValue LEFT , TileValue CENTER , TileValue RIGHT , TileValue DOWNLEFT , TileValue DOWN , TileValue DOWNRIGHT>
+struct print<Board<UPLEFT,UP,UPRIGHT,LEFT,CENTER,RIGHT,DOWNLEFT,DOWN,DOWNRIGHT>>
+{
+    using board = Board<UPLEFT,UP,UPRIGHT,LEFT,CENTER,RIGHT,DOWNLEFT,DOWN,DOWNRIGHT>;
+    
+    static void print_board()
+    {
+        cout << to_string(UPLEFT)   << " " << to_string(UP)     << " " << to_string(UPRIGHT)   << endl
+             << to_string(LEFT)     << " " << to_string(CENTER) << " " << to_string(RIGHT)     << ( PlayerWins<board,X>::result ? " Game over (X wins)" : ( PlayerWins<board,O>::result ? " Game over (O wins)" : "" ) ) << endl
+             << to_string(DOWNLEFT) << " " << to_string(DOWN)   << " " << to_string(DOWNRIGHT) << endl
+             << endl;
+    }
+};
+
+template<typename BOARDS>
+struct print_boards;
+
+template<typename HEAD , typename... TAIL>
+struct print_boards<dl32TypeList<HEAD,TAIL...>>
+{
+    static void print_board()
+    {
+        print<HEAD>::print_board();
+        
+        if(sizeof...(TAIL) > 0 )
+            print_boards<dl32TypeList<TAIL...>>::print_board();
+    }
+};
+
+template<>
+struct print_boards<dl32EmptyTypeList>
+{
+    static void print_board()
+    {
+    }
+};
+
+using init_board = Board< 
+                          X , e , O ,
+                          e , e , e,
+                          O , e , X
+                        >;
+
 
 int main()
 {
-
+    const TileValue next_turn = X;
+    using next_turn_wrapper = Tile<next_turn>;
+    using next_boards = typename GenerateNextBoards<init_board,next_turn_wrapper>::result;
+    print<init_board>::print_board();
+    cout << "Generated moves:" << endl << endl;
+    print_boards<next_boards>::print_board();
+    
     return 0;
 }
 
